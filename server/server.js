790e9cab2f6d98c1a91454985d0d625058db948a
Harpreet Singh <singhharpreet.chadha@gmail.com>
2018-01-06 19:37:49 +0530

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -10,7 +10,14 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 io.on('connection',function(socket){
   console.log('New user connected');
-
+  socket.emit('createMessage',{
+    from : 'admin',
+    text : 'Hi Thanks for joining the chatroom'
+  });
+  socket.broadcast.emit('createMessage',{
+    from : 'admin',
+    text : 'New user joined'
+  })
   socket.on('createMessage',(newMessage)=>{
     console.log('Create message',newMessage);
     io.emit('newMessage',{
@@ -18,6 +25,11 @@ io.on('connection',function(socket){
       text : newMessage.text,
       CreatedAt : new Date().getTime()
     })
+    // socket.broadcast.emit('newMessage',{
+    //   from : newMessage.from,
+    //   text : newMessage.text,
+    //   CreatedAt : new Date().getTime()
+    // })
   });
   socket.on('disconnect',()=>{
     console.log('User disconnected');
